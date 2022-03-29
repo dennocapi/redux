73e319d3d43048d66439326ba996b3b7e92e10ce
Dennis Mutuma <dennocapi@gmail.com>
2022-03-29 09:42:19 +0300

    Initial commit

---
+++ redux-starter/src/index.js
@@ -0,0 +1,23 @@
+import store from './store'
+import {
+    bugAdded,
+    bugResolved
+} from './actions'
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Store changed', store.getState())
+})
+
+unsubscribe()
+
+store.dispatch(bugAdded('Bug 1'))
+store.dispatch(bugResolved(1))
+
+store.dispatch({
+    type: actions.BUG_REMOVED,
+    payload: {
+        id: 1
+    }
+})
+
+console.log(store.getState())
